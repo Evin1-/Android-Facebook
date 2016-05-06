4c3fb4e6cc32f0c4988130d78b65e05285985212
Evin1- <edwin.hdz.12.0@gmail.com>
2016-05-06 17:58:23 -0400

    Add Facebook button to the main layout

--- app/src/main/java/com/example/simplefacebook/MainActivity.java
+++ app/src/main/java/com/example/simplefacebook/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.simplefacebook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTAG_";
+
+    private LoginButton mLoginButton;
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLoginButton = (LoginButton) findViewById(R.id.a_main_fb_btn);
+
+        mCallbackManager = CallbackManager.Factory.create();
+
+        mLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "onSuccess: " + loginResult);
+                Log.d(TAG, "onSuccess: " + loginResult.getAccessToken());
+                Log.d(TAG, "onSuccess: " + loginResult.getRecentlyDeniedPermissions());
+                Log.d(TAG, "onSuccess: " + loginResult.getRecentlyGrantedPermissions());
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
